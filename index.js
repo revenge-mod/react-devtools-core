507abf79ba839499c0df37a260e7cf02a3edc302
PalmDevs <git@palmdevs.me>
2024-10-11 17:54:48 +0700

    fix: revert to RTDC 5.3.1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import * as reactDevToolsCore from "react-devtools-core";
 
+// TODO: Fix 6.0.0, this is one step in the right direction
+// reactDevToolsCore.initialize({
+//     appendComponentStack: true,
+//     breakOnConsoleErrors: false,
+//     showInlineWarningsAndErrors: true,
+//     hideConsoleLogsInStrictMode: false,
+// });
+
 globalThis.__reactDevTools = {
-    version: __RDTC_VERSION, // Sync with package.json
-    exports: reactDevToolsCore
+    version: __RDTC_VERSION,
+    exports: reactDevToolsCore,
 };
---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
+import define from 'rollup-plugin-define';
+import commonJs from '@rollup/plugin-commonjs';
+import rdtPkg from './node_modules/react-devtools-core/package.json' with { type: 'json' };
+
+export default {
+    input: 'index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'iife',
+        compact: true,
+        intro: "var window = globalThis, self = globalThis, console = new Proxy({}, { get: () => () => void 0 }), setTimeout = (x) => x();"
+    },
+    plugins: [
+        nodeResolve(),
+        commonJs(),
+        define({
+            replacements: {
+                __RDTC_VERSION: `"${rdtPkg.version}"`
+            }
+        }),
+        esbuild({ minify: true })
+    ]
+};
