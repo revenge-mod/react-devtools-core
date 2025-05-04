ce364e8bb2c613f8f300967c19936f7640f55fe2
PalmDevs <git@palmdevs.me>
2025-05-05 01:43:43 +0700

    fix(build): wrong binary path

--- build.ts
+++ build.ts
@@ -89,8 +89,8 @@ function swcPlugin() {
 function hermesCPlugin({ flags }: { flags?: string[] } = {}) {
     const paths = {
         win32: 'hermesc.exe',
-        darwin: 'darwin/hermesc',
-        linux: 'linux/hermesc',
+        darwin: 'hermesc',
+        linux: 'hermesc',
     }
 
     if (!(process.platform in paths)) throw new Error(`Unsupported platform: ${process.platform}`)
